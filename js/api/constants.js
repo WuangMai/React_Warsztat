c40ec0b1ba6022cf0fbfeec7eefa4f5e17cd5726
wuang <wuangmai@gmail.com>
2021-09-28 11:42:52 +0200

    config

---
+++ js/api/constants.js
@@ -0,0 +1,2 @@
+export const API_KEY = "ad1603d5-cdb0-4750-afcd-8a03c1cf0b12";
+export const API_URL = "https://todo-api.coderslab.pl/api";
--- js/app.js
+++ js/app.js
@@ -1,8 +1,22 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import ReactDOM from "react-dom";
+import {getTasks} from "./api/tasks";
+import NewTask from "./components/NewTask";
+import Task from "./components/Task";
 
-function App() {
-  return <h1>Hello World</h1>
-}
+const App = () => {
+    const [tasks, setTasks] = useState([]);
+    useEffect(() => {
+        getTasks(setTasks);
+
+    }, [])
+    return (
+        <>
+            <NewTask/>
+            <Task/>
+        </>
+    );
+};
+
 
 ReactDOM.render(<App/>, document.querySelector("#app"));
